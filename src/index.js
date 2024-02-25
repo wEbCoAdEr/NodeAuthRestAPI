8738ef4918c7b9e4466c0710d9c48ccd0d7ecce0
webcoader <blackmanin062@gmail.com>
2024-02-25 17:12:49 +0600

    Refactor server initialization and termination process for improved manageability and graceful shutdown | Removed 'http' package dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // Loads required dependencies
 const mongoose = require('mongoose');
-const http = require('http');
 const config = require('./config');
 const app = require('./app');
-const { logger } = require('./utils');
+const {logger} = require('./utils');
 
-// Initiate server instance
-let server = http.createServer(app);
+// Initiate server variable
+let server;
 
 // Connect to the database
 mongoose.connect(config.DB_URI, {
@@ -17,10 +16,53 @@ mongoose.connect(config.DB_URI, {
   logger.info("Connected to MongoDB Database");
 
   // Start HTTP web server
-  server.listen(config.APP_PORT, () => {
-    logger.info(`Server is listening on http://127.0.0.1:${config.APP_PORT}`);
+  server = app.listen(config.APP_PORT, () => {
+    logger.info(`Server is listening on http://127.0.0.1:${config.APP_PORT} | Process ID: ${process.pid}`);
   });
-
-}).catch((error) => {
-  logger.error(error);
 });
+
+/**
+ * Terminate the application process gracefully with logging and exit code
+ * @function
+ * @param {string} reason - Reason for termination
+ * @param {string} [signal=''] - Termination signal
+ * @param {number} [exitCode=0] - Exit code for termination
+ * @returns {void}
+ */
+const terminateProcess = (reason, signal = '', exitCode = 0) => {
+  const message = reason === 'command' ? `Received ${signal} signal!` : 'An unexpected error occurred!';
+  logger.info(`${message} Shutting down the application...`);
+
+  if (server) {
+    server.close(() => logger.info('Application server closed'));
+  }
+
+  logger.info('Application shutdown process completed successfully');
+  process.exit(exitCode);
+}
+
+/**
+ * Handle unexpected errors gracefully
+ * @function
+ * @param {Error} error - The unexpected error
+ * @returns {void}
+ */
+const handleUnexpectedError = (error) => {
+  logger.error('An unexpected error occurred:', error);
+  terminateProcess('error', '', 1); // Use specific exit code for errors
+}
+
+// Register unexpected error event handlers
+process.on('uncaughtException', handleUnexpectedError);
+process.on('unhandledRejection', handleUnexpectedError);
+
+// Graceful termination handler on SIGTERM signal
+process.on('SIGTERM', () => {
+  terminateProcess('command', 'SIGTERM');
+});
+
+// Forceful termination handler on SIGINT signal
+process.on('SIGINT', () => {
+  terminateProcess('command', 'SIGINT');
+});
+
